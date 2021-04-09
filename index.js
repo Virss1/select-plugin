924ea1a3c18eca00a489ac7b74bb8cb8b4121dba
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-04-09 23:14:24 +0500

    index.js: add callback onSelect

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import './select/styles.scss';
 
 const select = new Select('#select', {
   placeholder: 'Выбери пожалуйста элемент',
+  // selectedId: '2',
   data: [
     { id: '1', value: 'React' },
     { id: '2', value: 'Angular' },
@@ -11,6 +12,9 @@ const select = new Select('#select', {
     { id: '5', value: 'Next' },
     { id: '6', value: 'Nest' },
   ],
+  onSelect(item) {
+    console.log('Selected item', item);
+  },
 });
 
 // select.select(4);
