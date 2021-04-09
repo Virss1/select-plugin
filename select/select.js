14e63755b4f5c91caa2b753cffd97d65a9ac234e
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-04-09 23:20:43 +0500

    select.js: Prettier formatting

--- select/select.js
+++ select/select.js
@@ -13,7 +13,7 @@ const getTemplate = (data = [], placeholder, selectedId) => {
       <li class="select__item ${cls}" data-type="item" data-id="${item.id}">${item.value}</li>
     `;
   });
-  
+
   return `
     <div class="select__input" data-type="input">
       <span data-type="value">${text}</span>
@@ -21,11 +21,11 @@ const getTemplate = (data = [], placeholder, selectedId) => {
     </div>
     <div class="select__dropdown">
       <ul class="select__list">
-        ${items.join('')}
+        ${items.join("")}
       </ul>
     </div>
   `;
-}
+};
 
 export class Select {
   constructor(selector, options) {
@@ -45,7 +45,7 @@ export class Select {
 
   #setup() {
     this.clickHandler = this.clickHandler.bind(this);
-    this.$el.addEventListener('click', this.clickHandler);
+    this.$el.addEventListener("click", this.clickHandler);
     this.$arrow = this.$el.querySelector('[data-type="arrow"]');
     this.$value = this.$el.querySelector('[data-type="value"]');
   }
@@ -55,42 +55,42 @@ export class Select {
     // ['input', 'arrow'].includes(type)
     if (type === 'input') {
       this.toggle();
-    } else if (type === 'item') {
+    } else if (type === "item") {
       const id = event.target.dataset.id;
       this.select(id);
     }
   }
 
   get isOpen() {
-    return this.$el.classList.contains('open');
+    return this.$el.classList.contains("open");
   }
 
   get current() {
-    return this.options.data.find(item => item.id === this.selectedId);
+    return this.options.data.find((item) => item.id === this.selectedId);
   }
 
   select(id) {
     this.selectedId = id;
     this.$value.textContent = this.current.value;
   }
-  
+
   toggle() {
     this.isOpen ? this.close() : this.open();
   }
 
   open() {
-    this.$el.classList.add('open');
-    this.$arrow.classList.remove('fa-chevron-down');
-    this.$arrow.classList.add('fa-chevron-up');
+    this.$el.classList.add("open");
+    this.$arrow.classList.remove("fa-chevron-down");
+    this.$arrow.classList.add("fa-chevron-up");
   }
 
   close() {
-    this.$el.classList.remove('open');
-    this.$arrow.classList.remove('fa-chevron-up');
-    this.$arrow.classList.add('fa-chevron-down');
+    this.$el.classList.remove("open");
+    this.$arrow.classList.remove("fa-chevron-up");
+    this.$arrow.classList.add("fa-chevron-down");
   }
 
   destroy() {
-    this.$el.removeEventListener('click', this.clickHandler);
+    this.$el.removeEventListener("click", this.clickHandler);
   }
 }
