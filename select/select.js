745d0a00af4c42965c04aa4be466ba92c63c9b87
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-04-09 20:28:20 +0500

    select.js: constructor modification
    add options acquiring from options object passed from index.js;
    add selectedId property (null by default);
    add render & setup private methods calling

--- select/select.js
+++ select/select.js
@@ -23,6 +23,13 @@ const getTemplate = (data = [], placeholder) => {
 export class Select {
   constructor(selector, options) {
     this.$el = document.querySelector(selector);
+    this.options = options;
+    this.selectedId = null;
+
+    this.#render();
+    this.#setup();
+  }
+
   #render() {
     const {placeholder, data} = this.options;
     this.$el.classList.add('select');
