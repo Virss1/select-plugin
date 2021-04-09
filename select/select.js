b7c8e5bc1b15a53a86953dd4c130f418755a6209
Ivan Riskov <44531490+Virss1@users.noreply.github.com>
2021-04-09 20:22:36 +0500

    select.js: add getTemplate method
    this method generates inner HTML of select including selected value (or placeholder) & all items gotten from options.data array

--- select/select.js
+++ select/select.js
@@ -1,3 +1,25 @@
+const getTemplate = (data = [], placeholder) => {
+  const text = placeholder ?? 'Placeholder по умолчанию';
+
+  const items = data.map(item => {
+    return `
+      <li class="select__item" data-type="item" data-id="${item.id}">${item.value}</li>
+    `;
+  });
+  
+  return `
+    <div class="select__input" data-type="input">
+      <span data-type="value">${text}</span>
+      <i class="fa fa-chevron-down" data-type="arrow"></i>
+    </div>
+    <div class="select__dropdown">
+      <ul class="select__list">
+        ${items.join('')}
+      </ul>
+    </div>
+  `;
+}
+
 export class Select {
   constructor(selector, options) {
     this.$el = document.querySelector(selector);
